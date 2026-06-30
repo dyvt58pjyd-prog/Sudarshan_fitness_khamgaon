567f8daf1d966d322d3704f326b7064d8b1bafbf
dyvt58pjyd-prog <dyvt58pjyd@privaterelay.appleid.com>
2026-06-30 19:49:33 +0530

    Allow Reception role to manually approve payments

--- Files/dashboard/admin/approve_payment.php
+++ Files/dashboard/admin/approve_payment.php
@@ -2,7 +2,7 @@
 require '../../include/db_conn.php';
 page_protect();
 
-if ($_SESSION['role'] !== 'super_admin' && $_SESSION['role'] !== 'owner') {
+if (!isset($_SESSION['role']) || !in_array($_SESSION['role'], ['super_admin', 'owner', 'reception'])) {
     die("Access Denied");
 }
 
