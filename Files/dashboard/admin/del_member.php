9f511651f5122ff372bf321a90b8e303960fa63d
dyvt58pjyd-prog <dyvt58pjyd@privaterelay.appleid.com>
2026-07-02 18:34:05 +0530

    Fix member deletion by resolving foreign key constraints

--- Files/dashboard/admin/del_member.php
+++ Files/dashboard/admin/del_member.php
@@ -5,10 +5,24 @@ page_protect();
 $msgid = $_POST['name'];
 if (strlen($msgid) > 0) {
     $msgid = mysqli_real_escape_string($con, $msgid);
-    mysqli_query($con, "DELETE FROM users WHERE userid='$msgid'");
+    
+    // Delete associated records first to prevent foreign key constraint failures
+    mysqli_query($con, "DELETE FROM enrolls_to WHERE uid='$msgid'");
+    mysqli_query($con, "DELETE FROM address WHERE id='$msgid'");
+    mysqli_query($con, "DELETE FROM health_status WHERE uid='$msgid'");
+    mysqli_query($con, "DELETE FROM attendance WHERE uid='$msgid'");
+    
+    // Now delete the user and admin accounts
+    $delete_user = mysqli_query($con, "DELETE FROM users WHERE userid='$msgid'");
     mysqli_query($con, "DELETE FROM admin WHERE username='$msgid' AND role='member'");
-    echo "<html><head><script>alert('Member Deleted');</script></head></html>";
-    echo "<meta http-equiv='refresh' content='0; url=view_mem.php'>";
+    
+    if ($delete_user) {
+        echo "<html><head><script>alert('Member Deleted');</script></head></html>";
+        echo "<meta http-equiv='refresh' content='0; url=view_mem.php'>";
+    } else {
+        echo "<html><head><script>alert('ERROR! Delete Operation Unsuccessful');</script></head></html>";
+        echo "error".mysqli_error($con);
+    }
 } else {
     echo "<html><head><script>alert('ERROR! Delete Opertaion Unsucessfull');</script></head></html>";
    echo "error".mysqli_error($con);
