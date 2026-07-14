312069e9fde1a5049f8baa29a9ca4650d9a7b80c
dyvt58pjyd-prog <dyvt58pjyd@privaterelay.appleid.com>
2026-07-14 07:28:18 +0530

    Enhance fix script to show affected rows

--- Files/fix_tejal.php
+++ Files/fix_tejal.php
@@ -1,8 +1,9 @@
 <?php
 require 'include/db_conn.php';
-$query = "UPDATE enrolls_to SET paid_date = '2026-07-13' WHERE uid = '165' AND paid_date = '2026-07-14'";
+$query = "UPDATE enrolls_to SET paid_date = '2026-07-13' WHERE uid = '165' AND paid_date >= '2026-07-14'";
 if(mysqli_query($con, $query)) {
-    echo "Successfully updated TEJAL RATHOD (ID: 165) transaction date to yesterday (July 13).";
+    $rows = mysqli_affected_rows($con);
+    echo "SUCCESS: Updated $rows record(s) for TEJAL RATHOD (ID: 165) back to July 13th.";
 } else {
     echo "Error updating: " . mysqli_error($con);
 }
